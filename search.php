b0bf91789a17a4ed33cda8c8ca841f07495df000
Jakob <jakob.grannas@intelliplan.se>
2014-01-27 10:28:10 +0100

    Added some comment section styling

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ Template Name: Search Results Page
 				<ul class="post-listing reset-box-model bare-list">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<li>
-						<h3><a href="#"><?php the_title(); ?></a></h3>
+						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<p class="post-body">
 							<?php the_excerpt(); ?>
 						</p>
