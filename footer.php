47d6e51a342226e8178e88672c8729cb57edf01a
Jakob <jakob.grannas@intelliplan.se>
2014-04-15 12:27:52 +0200

    Removed bottom row from footer and moved logo to left

--- footer.php
+++ footer.php
@@ -1,20 +1,17 @@
 			<footer class="boxsized footer">
 				<a href="#" class="scroll-top-btn" id="scroll-top-btn" title="Scroll to top"><span class="fa"></span></a>
-				<div class="vcard top-row">
-					<div class="footer-block copyright">
-						<p>&copy; <?php echo date('Y'); ?></p>
-						<p class="url fn"><?php bloginfo( 'name' ); ?></p>
+				<div class="vcard container">
+					<div class="footer-block">
+						<h2 class="logo">
+							<a href="<?php echo home_url(); ?>" class="bare-link" rel="nofollow">
+								<span class="row1">Patrik</span>
+								<span class="row2">Blom</span>
+							</a>
+						</h2>
+						<small class="copyright">&copy; <?php echo date('Y'); ?></small>
 					</div>
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>
 					<?php endif; ?>
-				</div>
-				<div class="bottom-row">
-					<h2 class="logo">
-						<a href="<?php echo home_url(); ?>" class="bare-link" rel="nofollow">
-							<span class="row1">Patrik</span>
-							<span class="row2">Blom</span>
-						</a>
-					</h2>
 				</div>
 			</footer>
 		</div>
